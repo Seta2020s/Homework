004ef2d9b7b43d7e9d9b9705196542afbe088d49
Sveta <seta2020s@gmail.com>
2023-07-06 16:46:33 +0700

    проверяет четность

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу,которая принимает на вход три числа и выдает максимальное из этих чисел
+
+
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+
+int maxNumber = Math.Max(number1, Math.Max(number2, number3));
+Console.WriteLine($"Максимальное число: {maxNumber}");
---
+++ task4/task6/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу,которая на вход принимает число и выдает,является ли оно четным
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число является четным.");
+}
+else
+{
+    Console.WriteLine("Число не является четным.");
+}
