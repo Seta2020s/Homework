3d73447ea6a525a8c0559f51fafdfdd9eac323d5
Sveta <seta2020s@gmail.com>
2023-07-06 16:31:25 +0700

    выдает большее и меньшее из двух чисел

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу,которая на вход примет два числа и выдает,какое число большее,а какое меньшее
+
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"Большее число : {number1}");
+    Console.WriteLine($"Меньшее число : {number2}");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"Большее число: {number2}");
+    Console.WriteLine($"Меньшее число: {number1}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
